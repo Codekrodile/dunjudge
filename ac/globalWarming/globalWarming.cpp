54748066a24d7198cb16f0bd37c5c4e719986f2d
Lucas Lim <48521605+Codekrodile@users.noreply.github.com>
2020-02-14 13:17:46 +0800

    Add files via upload

--- ac/globalWarming/globalWarming.cpp
+++ ac/globalWarming/globalWarming.cpp
@@ -1,14 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void showpq(priority_queue <int> pq) {
-	cout << "\npq: ";
-	while (!pq.empty()) {
-		cout << pq.top() << ' ';
-		pq.pop();
-	}
-}
-
 int gw (int N, int *H) {
 	priority_queue <int> peak;
 	priority_queue <int> valley;
@@ -16,51 +8,38 @@ int gw (int N, int *H) {
 	int n = 0;
 	while (n < N) { //go through array
 		if (n == 0)  { //first element
-			cout << "\nfirst element";
-			
 			while (H[n] == H[n+1] && n < N) {
 				n++;
 			}
 			
 			if (H[n] > H[n+1]) {
 				peak.push(H[n]);
-				cout << "\npeak first push: " << H[n];
 			}
-
 			n++; //move on to the 2nd element
 		}
 		
 		else if (H[n] < H[n-1]) { //finding valley
 			while (H[n] <= H[n-1] && n < N) {
 				n++;
-                        }
-   
-                        if (n != N) {
-			        cout << "\nvalley: " << H[n-1];
-			        valley.push(H[n-1]);
-                        }
+			}
+			
+			if (n != N) {
+		    	valley.push(H[n-1]);
+			}
 		}
 		
 		else if (H[n] > H[n-1]) {
 			while (H[n] >= H[n-1] && n < N) {
 				n++;
 			}
-			cout << "\npeak: " << H[n-1];
 			peak.push(H[n-1]);
 		}
 		
 		else if (H[n] == H[n-1]) {
 			n++;
-			cout << "\nskip";
 		}
-		
-		cout << "\n" << n;
-
 	}
 
-	showpq(peak);
-	showpq(valley);
-	
 	//start from flood
 	int total = 0, max = 1, lvl;
 
@@ -73,16 +52,12 @@ int gw (int N, int *H) {
 			lvl = valley.top();
 		}
 		
-		cout <<"\nlevel: " << lvl << "\n";
-		
 		while (peak.top() == lvl && !peak.empty()) {
-			cout << "test1+ ";
 			peak.pop();
 			total++;
 		}
 		
 		while (valley.top() == lvl && !valley.empty()) {
-			cout << "test2- ";
 			valley.pop();
 			total--;
 		}
@@ -90,24 +65,20 @@ int gw (int N, int *H) {
 		if (total > max) {
 			max = total;
 		}
-		cout << "\ntotal: " << total;
-		cout << "\nmax: " << max;
 	}	
-	showpq(peak);
-	showpq(valley);
-	
-	cout << "\ntotal: " << total;
-	cout << "\nmax: " << max;
+
+	cout << max;
 	
 	return 0;
 }
 
 int main() {
-	int N; cin >> N;
-	int H[N];
-	for (int i = 0; i < N; i++) {
-		cin >> H[i];
-	}
+    long long int N;
+    cin >> N;
+    int H[N];
+    for (int i = 0; i < N; i++) {
+        cin >> H[i];
+    }
 	gw(N, H);
 	
 	return 0;
